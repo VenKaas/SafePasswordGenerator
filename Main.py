059d6209d10f322a63d32b457c7f1c7194633bd0
Vladislav Knysh <knyshvladislav@yandex.ru>
2024-09-17 14:58:38 +0500

    v2

--- Main.py
+++ Main.py
@@ -1,41 +1,47 @@
 #Создаём генератор безопасных паролей
 
 import random
-import string
 
-# Константы для генерации пароля
-DIGITS = '0123456789'
-LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
-UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-PUNCTUATION = '!#$%&*+-=?@^_'
+digits = '0123456789'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+punctuation = '!#$%&*+-=?@^_'
+
 chars = ''
 
-# Считывание данных
-count_password = int(input('Укажите количество паролей: '))
-length_password = int(input('Укажите длину пароля: '))
-include_digits = input('Включать цифры? (y/n):').strip()
-include_high_letters = input('Включать буквы? (y/n):').strip()
-include_low_letters = input('Включать строчные буквы? (y/n):').strip()
-include_punctuation = input('Включать знаки? (y/n):').strip()
-exclude_unusual = input('Исключать необычные символы? (y/n):').strip()
+# Считывание пользовательских данных:
 
-# Сценарии генарации пароля
-if include_digits == 'y':
-    chars += DIGITS
-if include_high_letters == 'y':
-    chars += UPPERCASE_LETTERS
-if include_low_letters == 'y':
-    chars += LOWERCASE_LETTERS
-if include_punctuation == 'y':
-    chars += PUNCTUATION
-if exclude_unusual == 'y':
+n = int(input('Введите количество паролей для генерации: '))
+length = int(input('Введите длину пароля: '))
+add_digit = input('Включить цифры? (д = да, н = нет) ').strip()
+add_lowercase = input('Включить прописные буквы? (д = да, н = нет) ').strip()
+add_uppercase = input('Включить строчные буквы? (д = да, н = нет) ').strip()
+add_punctuation = input('Включить символы, такие как !#$%&*+-=?@^_? (д = да, н = нет) ').strip()
+remove_badsymbols = input('Исключить символы il1Lo0O? (д = да, н = нет)').strip()
+
+# Настройка генерируемых паролей:
+
+if add_digit.lower() == 'д': # добавляем цифры
+    chars += digits
+if add_lowercase.lower() == 'д': # добавляем строчные буквы
+    chars += lowercase_letters
+if add_uppercase.lower() == 'д': # добавляем прописные буквы
+    chars += uppercase_letters
+if add_punctuation.lower() == 'д': # добавляем специальные символы
+    chars += punctuation
+if remove_badsymbols.lower() == 'д': # удаляем специальные символы
     for c in 'il1Lo0O':
         chars = chars.replace(c, '')
 
-# Генерация пароля
-for _ in range(count_password):
-    password = ''
-    for _ in range(length_password):
-        password += random.choice(chars)
-    print(password)
+# Функция генерации пароля:
 
+def generate_password(length, chars):
+    password = ''
+    for j in range(length): # генерируем пароль
+        password += random.choice(chars)
+    return password
+
+# Генерация нужного количества паролей:
+
+for _ in range(n):
+    generate_password(length, chars)
